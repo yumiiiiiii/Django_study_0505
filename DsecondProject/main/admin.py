34d2a56a1c76dd3196feeb97834feb5ca0e09e74
aqswa <69039161+aqswa@users.noreply.github.com>
2022-05-31 20:04:00 +0900

    user auth

--- DsecondProject/main/admin.py
+++ DsecondProject/main/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from main.models import Recipe, Ingredient, Comment, HashTag
+from account.models import CustomUser
 
 # Register your models here.
 @admin.register(Recipe)
 @admin.register(Ingredient)
 @admin.register(Comment)
 @admin.register(HashTag)
+@admin.site.register(CustomUser)
 
 class RecipeAdmin(admin.ModelAdmin):
     posting_list = ('id', 'title', 'content', 'ingredients')
